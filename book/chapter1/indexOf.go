6da80c0c6e25988e93059e99b03f165d0197b5f1
Omar Ayman <se.omarayman@gmail.com>
2023-12-22 07:57:58 +0200

    improve indexOf

--- book/chapter1/indexOf.go
+++ book/chapter1/indexOf.go
@@ -2,7 +2,7 @@ package chapter1
 
 func IndexOf(t, s string) int {
 	idx := -1
-	for i := 0; i < len(t); i++ {
+	for i := 0; i < len(t) - len(s); i++ {
 		idx = i
 		for j := 0; j < len(s); j++ {
 			if s[j] != t[i+j] {
