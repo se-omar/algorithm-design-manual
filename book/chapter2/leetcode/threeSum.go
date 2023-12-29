32218a70c1e310f801fd19877321d900dcdb4761
Omar Ayman <se.omarayman@gmail.com>
2023-12-29 21:28:18 +0200

    solve threeSum

---
+++ book/chapter2/leetcode/threeSum.go
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/3sum/
+
+package chapter2
+
+import (
+	"sort"
+)
+
+func threeSum(nums []int) [][]int {
+	ans := [][]int{}
+
+	sort.Ints(nums)
+
+	for i := 0; i < len(nums); i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		l := i + 1
+		r := len(nums) - 1
+
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+			if sum < 0 {
+				l++
+			} else if sum > 0 {
+				r--
+			} else {
+				ans = append(ans, []int{nums[i], nums[l], nums[r]})
+				l++
+				for nums[l] == nums[l-1] && l < r {
+					l++
+				}
+			}
+
+		}
+
+	}
+
+	return ans
+
+}
