bd513ad4707f6840816aee4dc3cca95322d90ca4
Omar Ayman <se.omarayman@gmail.com>
2024-01-06 17:19:24 +0200

    remove from linked list

--- book/chapter3/linkedList.go
+++ book/chapter3/linkedList.go
@@ -5,13 +5,18 @@ type LinkedList struct {
 	next *LinkedList
 }
 
+var NULL = LinkedList{
+	item: nil,
+	next: nil,
+}
+
 func search(item any, list *LinkedList) any {
 	if list == nil {
 		return nil
 	}
 
 	if list.item == item {
-		return item
+		return list
 	} else {
 		return search(item, list.next)
 	}
@@ -24,4 +29,25 @@ func insert(item any, list *LinkedList) {
 	}
 
 	*list = newL
+}
+
+func remove(doomedNode *LinkedList, list *LinkedList) {
+	pred := predecessor(doomedNode, list)
+	if pred == nil {
+		return
+	} else {
+		pred.next = doomedNode.next
+	}
+}
+
+func predecessor(doomedNode *LinkedList, origList *LinkedList) *LinkedList {
+	if origList == nil || origList.next == nil {
+		return &LinkedList{}
+	}
+
+	if origList.next == doomedNode {
+		return origList
+	} else {
+		return predecessor(doomedNode, origList.next)
+	}
 }
