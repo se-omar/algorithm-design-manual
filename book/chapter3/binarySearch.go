f59ff289fae5f900683eee3d6cd010049acbef87
Omar Ayman <se.omarayman@gmail.com>
2024-01-07 14:47:23 +0200

    binarySearch attempt 2

--- book/chapter3/binarySearch.go
+++ book/chapter3/binarySearch.go
@@ -1,16 +1,19 @@
 package chapter3
 
-func BinarySearch(item int, arr []int, low, high int) int {
-	if low > high {
+func BinarySearch(arr []int, item int, low, high int) int {
+	if low >= high {
 		return -1
 	}
+
 	mid := low + (high-low)/2
+
 	if arr[mid] < item {
-		return BinarySearch(item, arr, mid+1, high)
-	} else if arr[mid] > item {
-		return BinarySearch(item, arr, low, mid-1)
+		return BinarySearch(arr, item, mid+1, high)
+	}
+
+	if arr[mid] > item {
+		return BinarySearch(arr, item, low, mid-1)
 	} else {
 		return mid
 	}
-
 }
--- main.go
+++ main.go
@@ -1,3 +1,12 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"tadd/book/chapter3"
+)
+
+func main() {
+	arr := []int{1, 2, 4, 8, 31, 66}
+
+	fmt.Println(chapter3.BinarySearch(arr, 660, 0, len(arr)))
+}
