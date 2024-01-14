b36010593b60434b04fb7916ee56ef6e391b9191
Omar Ayman <se.omarayman@gmail.com>
2024-01-14 20:46:23 +0200

    better sorting sequence

--- book/chapter3/exercises/e310.go
+++ book/chapter3/exercises/e310.go
@@ -1,6 +1,9 @@
 package chapter3
 
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 func isAnagram(s string, t string) bool {
 	// sort both strings O(nlogn) O(1)
@@ -8,13 +11,13 @@ func isAnagram(s string, t string) bool {
 		return false
 	}
 
-	bytesS := []byte(s)
-	bytesT := []byte(t)
-	sort.Slice(bytesS, func(i, j int) bool { return bytesS[i] < bytesS[j] })
-	sort.Slice(bytesT, func(i, j int) bool { return bytesT[i] < bytesT[j] })
-	s = string(bytesS)
-	t = string(bytesT)
+	x := strings.Split(s, "")
+	sort.Strings(x)
+	s = strings.Join(x, "")
 
+	x = strings.Split(t, "")
+	sort.Strings(x)
+	t = strings.Join(x, "")
 
 	return s == t
 }
