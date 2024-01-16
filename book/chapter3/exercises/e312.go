c7373c2b326063c58247a8a56749bc0dbf298dc5
Omar Ayman <se.omarayman@gmail.com>
2024-01-16 03:15:16 +0200

    solve exercise 3.12 iteratively

--- book/chapter3/exercises/e312.go
+++ book/chapter3/exercises/e312.go
@@ -6,13 +6,13 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-func maxDepth(root *TreeNode) int {
+func maxDepthRecur(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
 
-	countL := 1 + maxDepth(root.Left)
-	countR := 1 + maxDepth(root.Right)
+	countL := 1 + maxDepthRecur(root.Left)
+	countR := 1 + maxDepthRecur(root.Right)
 
 	if countL > countR {
 		return countL
@@ -20,4 +20,43 @@ func maxDepth(root *TreeNode) int {
 
 	return countR
 
+}
+
+type stack []stackItem
+
+type stackItem struct {
+	node  *TreeNode
+	level int
+}
+
+func (s *stack) pop() stackItem {
+	el := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return el
+}
+
+func (s *stack) push(el stackItem) {
+	*s = append(*s, el)
+}
+
+func maxDepthIter(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	maxLevel := 1
+	stack := stack{stackItem{root, 1}}
+
+	for len(stack) > 0 {
+		newItem := stack.pop()
+		if newItem.node == nil {
+			continue
+		}
+
+		maxLevel = max(maxLevel, newItem.level)
+		stack.push(stackItem{newItem.node.Left, newItem.level + 1})
+		stack.push(stackItem{newItem.node.Right, newItem.level + 1})
+	}
+
+	return maxLevel
 }
