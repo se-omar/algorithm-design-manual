b76045a4d90e326f439bfbbabfd1ac1ea88ba981
Omar Ayman <se.omarayman@gmail.com>
2024-01-20 15:31:47 +0200

    sum size k sliding window approach

--- book/chapter3/sumSizeK.go
+++ book/chapter3/sumSizeK.go
@@ -1,17 +1,26 @@
 package main
 
-func sumSizeK(nums []int, k int) int {
-	// naive approach
+func SumSizeK(nums []int, k int) []int {
+	// sliding window
+	total := []int{}
 
-	total := 0
-	arrSum := 0
+	arrSum := sum(nums[:k])
+	total = append(total, arrSum)
 
-	for i := 0; i < len(nums)-k; i++ {
-		for j := i; j < k; j++ {
-			arrSum += nums[j]
-		}
-		total += arrSum
+	for i := 1; i <= len(nums)-k; i++ {
+		arrSum = arrSum - nums[i-1] + nums[i+k-1]
+		total = append(total, arrSum)
 	}
 
-	return arrSum
+	return total
 }
+
+func sum(nums []int) int {
+	sum := 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	return sum
+}
+
