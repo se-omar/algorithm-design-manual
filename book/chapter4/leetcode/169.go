c0dda40a5cc5cb73a4b25a9bdae1708f531668ff
Omar Ayman <se.omarayman@gmail.com>
2024-02-09 19:03:03 +0200

    improve 169 again

--- book/chapter4/leetcode/169.go
+++ book/chapter4/leetcode/169.go
@@ -1,17 +1,19 @@
 package leetcode
 
 func majorityElement(nums []int) int {
-	count, curr := 0, nums[0] 
+	count, curr := 0, 0
 
-	for i := 1; i < len(nums); i++ {
+	for i := 0; i < len(nums); i++ {
+		if count == 0 {
+			curr = nums[i]
+		}
+
 		if nums[i] == curr {
 			count++
-		} else if count == 0 {
-			curr = nums[i]
 		} else {
 			count--
 		}
 	}
-	 
+
 	return curr
 }
