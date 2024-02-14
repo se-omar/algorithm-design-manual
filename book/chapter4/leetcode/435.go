ee3f0611319a4b892418cd6dbcd0701ab6f7989d
Omar Ayman <se.omarayman@gmail.com>
2024-02-14 17:34:29 +0200

    improve 435

--- book/chapter4/leetcode/435.go
+++ book/chapter4/leetcode/435.go
@@ -8,24 +8,17 @@ func eraseOverlapIntervals(intervals [][]int) int {
 	}
 
 	slices.SortFunc(intervals, func(a, b []int) int { return a[0] - b[0] })
-	output := []int{intervals[0][0], intervals[0][1]}
+	prevEnd := intervals[0][1]
 	count := 0
 
 	for i := 1; i < len(intervals); i++ {
-		lastEnd := output[1]
 		currStart := intervals[i][0]
 		currEnd := intervals[i][1]
-		if currStart < lastEnd {
+		if currStart < prevEnd {
 			count++
-			newOp := []int{}
-			if lastEnd < currEnd {
-				newOp = output
-			} else {
-				newOp = []int{currStart, currEnd}
-			}
-			output = newOp
+			prevEnd = min(prevEnd, currEnd)
 		} else {
-			output = []int{currStart, currEnd}
+			prevEnd = currEnd
 		}
 	}
 
